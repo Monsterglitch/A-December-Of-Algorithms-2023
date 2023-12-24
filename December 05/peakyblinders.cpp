d4a7d978b9ba969bcfc4fab47008fe07de408ef7
pratheek <pratheekmanimaran@gmail.com>
2023-12-24 10:01:15 +0530

    December 05

--- December 05/peakyblinders.cpp
+++ December 05/peakyblinders.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main() {
-    int i, n, sum, avg;
-    cout<<"Enter no. thieves: ";
-    cin>>n;
-    int *stln = new int[n]();
-    cout<<"Enter amounts stolen by each thief sequentially: ";
-    for(i = 0; i < n; i++) {
-            cin>>stln[i];
-            sum += stln[i];
+    int i, n, sum = 0, avg;
+    cout << "Enter no. thieves: ";
+    cin >> n;
+    int stln[n];
+    cout << "Enter amounts stolen by each thief sequentially: ";
+    for (i = 0; i < n; i++) {
+        cin >> stln[i];
+        sum += stln[i];
     }
     avg = sum / n;
-    cout<<avg;
-    i = 0, sum = 0;
-    while(i < n) {
+    i = 0, sum = avg;
+    while (i < n) {
         sum += (stln[i] > avg) ? stln[i] : 0;
+        i++;
     }
-    cout<<endl<<sum;
+    cout<<"Sum of elements greater than or equal to average: "<<sum;
+    return 0;
 }
-
